e6ab07a790b9345fbc1508247cb10f85b7ebf736
Diego Sousa <diegosous@gmail.com>
2020-08-30 20:14:13 -0300

    Implementando Classe Tabuleiro

---
+++ src/br/dev/diego/cm/Aplicacao.java
@@ -0,0 +1,19 @@
+package br.dev.diego.cm;
+
+import br.dev.diego.cm.model.Tabuleiro;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		
+		tabuleiro.abrir(3, 3);
+		tabuleiro.alternarMarcacao(4, 4);
+		tabuleiro.alternarMarcacao(4, 5);
+		
+		System.out.println(tabuleiro);
+		
+	}
+	
+}

519ff696ee0b7a9d235b756b7534779e2fc6cd62
Diego Sousa <diegosous@gmail.com>
2020-08-30 21:17:51 -0300

    Exibindo campos com mina ao finalizar o jogo

--- src/br/dev/diego/cm/model/Tabuleiro.java
+++ src/br/dev/diego/cm/model/Tabuleiro.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+import br.dev.diego.cm.exception.ExplosaoException;
+
 public class Tabuleiro {
 
 	private int linhas;
@@ -23,9 +25,20 @@ public class Tabuleiro {
 	}
 	
 	public void abrir(int linha, int coluna) {
+		try {
+			
+			campos.parallelStream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+			.findFirst()
+			.ifPresent(c -> c.abrir());
+			
+		} catch (ExplosaoException e) {
+			campos.forEach(c -> c.setAberto(true));
+			throw e;
+		}
+		
 		campos.parallelStream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
 		.findFirst()
-		.ifPresent(c -> c.abrir());;
+		.ifPresent(c -> c.abrir());
 	}
 	
 	public void alternarMarcacao(int linha, int coluna) {
